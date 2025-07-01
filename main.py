cf73701d96d1b8d71e066460d6ecc85253505bf6
jorgeav527 <jorgeav527@gmail.com>
2025-06-30 21:50:03 -0500

    fet: get all posts

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 from flask import Flask, render_template
+import sqlite3
+
+def get_db_connection():
+    conn = sqlite3.connect("database.db")
+    conn.row_factory = sqlite3.Row
+    return conn
 
 app = Flask(__name__)
 
@@ -9,6 +15,14 @@ def index():
 @app.route("/home", methods=["GET"])
 def home():
     return render_template("home.html")
+
+@app.route("/post", methods=["GET"])
+def get_all_post():
+    conn = get_db_connection()
+    posts = conn.execute('SELECT * FROM posts;').fetchall()
+    return render_template("post/post_list.html", posts=posts)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
